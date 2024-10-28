92aadc7e65188c24c04a0c2632e88a3d015bb830
Montassar90 <montassar.bahria0390@gmail.com>
2024-10-28 21:29:26 +0100

    Modified TestsBase class code

--- src/test/java/com/e2eTests/paraBankDemo/utils/TestsBase.java
+++ src/test/java/com/e2eTests/paraBankDemo/utils/TestsBase.java
@@ -4,55 +4,55 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class TestsBase {
 
-	private static WebDriver driver;
-	private ConfigFileReader configFileReader = new ConfigFileReader();
+    // Static WebDriver instance, shared across tests.
+    private static WebDriver driver;
+    private ConfigFileReader configFileReader = new ConfigFileReader(); // Reads configuration settings
 
-	@Before
+    @Before
+    public void setup() {
+        // Gets the browser type from system properties, defaulting to "chrome"
+        String browser = System.getProperty("browser", "chrome");
+        
+        // Initializes the WebDriver based on the specified browser
+        driver = initializeDriver(browser);
+        
+        // Maximizes the browser window for better visibility during tests
+        driver.manage().window().maximize();
+        
+        // Opens the base URL specified in the configuration file
+        driver.get(configFileReader.getProperties("baseUrl"));
+    }
 
-	public void setup() {
-		String browser = System.clearProperty("browser");
-		if (browser == null) {
-			browser = "chrome";
-		}
+    private WebDriver initializeDriver(String browser) {
+        // Switch statement to select the WebDriver based on the browser type
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                return new ChromeDriver(); // Returns Chrome WebDriver instance
+            case "firefox":
+                return new FirefoxDriver(); // Returns Firefox WebDriver instance
+            case "edge":
+                return new EdgeDriver(); // Returns Edge WebDriver instance
+            default:
+                // Throws an exception if the browser type is not supported
+                throw new IllegalArgumentException("Browser \"" + browser + "\" is not supported");
+        }
+    }
 
-		switch (browser) {
-		case "chrome":
-			driver = new ChromeDriver();
-			driver.manage().window().maximize();
-			driver.get(configFileReader.getProperties("baseUrl"));
-			break;
-		case "firefox":
-			driver = new FirefoxDriver();
-			driver.manage().window().maximize();
-			driver.get(configFileReader.getProperties("baseUrl"));
-			break;
-		case "edge":
-			driver = new EdgeDriver();
-			driver.manage().window().maximize();
-			driver.get(configFileReader.getProperties("baseUrl"));
-			break;
+   @After
+    public void tearDown() {
+        // Closes the WebDriver if it is still active
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
-		default:
-			throw new IllegalArgumentException("Browser \"" + browser + "\"is not supported");
-		}
-	}
-
-	@After
-	public void tearDown() {
-		if (driver != null) {
-			driver.quit();
-		}
-	}
-
-	public static WebDriver getDriver() {
-		return driver;
-	}
-
+    // Returns the WebDriver instance to other classes if needed
+    public static WebDriver getDriver() {
+        return driver;
+    }
 }
