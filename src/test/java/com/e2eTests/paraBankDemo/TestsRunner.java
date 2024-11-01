2bd79eb6cab8b75a72888a1eace148d676ebdeb4
Montassar90 <montassar.bahria0390@gmail.com>
2024-11-01 20:06:32 +0100

    Added extent report

--- src/test/java/com/e2eTests/paraBankDemo/TestsRunner.java
+++ src/test/java/com/e2eTests/paraBankDemo/TestsRunner.java
@@ -7,6 +7,7 @@ import io.cucumber.testng.CucumberOptions.SnippetType;
 
 
 @CucumberOptions(features = { "src/spec/features" },
+plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 tags = (""), 
 monochrome = true, 
 snippets = SnippetType.CAMELCASE)
